2dae2b188c2d79afe7b5ee66918e7722dbc06a41
SimonPegasus <36254494+pegabooru@users.noreply.github.com>
2022-01-16 15:00:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d')
+const c = canvas.getContext('2d') // Context for a 2D canvas, use c.something() to call a canvas function like drawing and stuff
 
-var mdl = new Image()
+var plySprite = new Image()
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-class Player {
-    constructor(mdl, x, y, s, dir, frame) {
+class Player { // Local player
+    constructor(user, x, y, s, dir, frame) { // user = player's username, x,y = ply coords, s = speed, dir = direction facing, frame = current animation frame
         this.x = x
         this.y = y
         this.speed = s
         this.direction = dir
 
-        this.mdl = mdl
+        this.user = user
         this.frame = frame
     }
 
-    draw(velX, velY) {
-        if (this.frame > 15) {this.frame = 0}
-        this.x += velX
-        this.y += velY
-        var X = this.x
-        var Y = this.y
-        if (velX == 0 && velY == 0) {mdl.src = 'user/'+this.mdl+'/stand/'+this.mdl+'-'+this.direction+'.png'
+    draw(velX, velY) { // Draw the player
+        if (this.frame > 15) {this.frame = 0} // Loop frames
+        this.x += velX // Add velocityX
+        this.y += velY // Add velocityY
+        if (velX == 0 && velY == 0) {plySprite.src = 'user/'+this.user+'/stand/'+this.user+'-'+this.direction+'.png' // Standing still sprite when ply not moving
             this.frame = 0
         }
-        else {mdl.src = 'user/'+this.mdl+'/trot/'+this.mdl+'-'+this.direction+'-'+'trot'+Math.floor(this.frame)+'.png'}
+        else {plySprite.src = 'user/'+this.user+'/trot/'+this.user+'-'+this.direction+'-'+'trot'+Math.floor(this.frame)+'.png'} // When ply moving and on ground, trot
         c.beginPath()
-        c.drawImage(mdl, X, Y)
+        c.drawImage(plySprite, this.x, this.y) // Draw image onto canvas
         this.frame += 0.33
     }
 
-    update() {
+    update() { // Updates player data
         var velX = 0
         var velY = 0
 
-        if (keys['SHIFT']) {this.speed = 2} else {this.speed = 1}
-        if (keys['S']) {
+        // Obviously if you need more keybinds for stuff just copy one of the if statements
+        if (keys['SHIFT']) {this.speed = 2} else {this.speed = 1} // Sprint
+        if (keys['S']) { // Down
             velY = this.speed
         }
-        if (keys['W']) {
+        if (keys['W']) { // Up
             velY = -this.speed
         }
-        if (keys['D']) {
+        if (keys['D']) { // Right
             velX = this.speed
             this.direction = 'right'
         }
-        if (keys['A']) {
+        if (keys['A']) { // Left
             velX = -this.speed
             this.direction = 'left'
         }
@@ -63,17 +62,17 @@ keys = []
 
 const ply = new Player('simon', x, y, 1, 'right', 0)
 
-function animate() {
+function animate() { // Constant game loop, update frames, etc
     c.clearRect(0, 0, canvas.width, canvas.height)
     ply.update()
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate) // Loop back to animate function
 }
 
-addEventListener('keydown', function (e) {
+addEventListener('keydown', function (e) { // Keys down
     keys[e.key.toUpperCase()] = true
 })
-addEventListener('keyup', function (e) {
+addEventListener('keyup', function (e) { // Keys up
     keys[e.key.toUpperCase()] = false
 })
 
-animate()
+animate() // Run the whole thangy
